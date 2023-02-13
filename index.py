2585c72da3bfa6898819763e1154e417bf7ac437
JMwagunda <wagundamarc@gmail.com>
2023-02-14 00:26:41 +0300

    commi message

--- index.py
+++ index.py
@@ -4,28 +4,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("my_data.csv")
+data_2007 = data[data['year'] ==2007]
+
+df = pd.Dataframe(data)
 
 #Function to plot the violin plots
 def violinplots(df):
     sns.set_style('White grid')
     plt.figure(figsize=(10, 5))
     
-    plt.subplot(120)
+    plt.subplot(131)
     sns.violinplot(x='lifeExp', data=df)
     plt.title('LIFE EXPECTANCY')
     
-    plt.subplot(121)
+    plt.subplot(132)
     sns.violinplot(x='pop', data=df)
     plt.title('POPULATION')
     
-    plt.subplot(122)
-    sns.violinplot(x='gdppercap', data=df)
+    plt.subplot(133)
+    sns.violinplot(x='gdpPercap', data=df)
     plt.title('GDP per Capita')
     
     plt.tight_layout()
     plt.show()
     
     #Streamlit Representation
-    #Andika admission yako apo kwa brackets
-    st.title("Kenneth Ruto\nSCT211-0744/2021\nData Visualization with Violin Plots")
+    st.title("JUDD MARC ODOYO\nSCT211-0744/2021\nData Visualization with Violin Plots")
     violinplots(df)
